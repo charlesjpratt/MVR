b21f96d29f4c9d01d009c72bc73163677fad63a2
Charles J Pratt <charlesjpratt>
2019-09-07 13:46:05 -0400

    Created ship object.

---
+++ Assets/MainMananger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMananger : MonoBehaviour
+{
+    public Ship player;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        player = new Ship(0,0, "Player Ship", "ship");
+        player.Initialize();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+       
+       player.Update();
+
+    }
+}
---
+++ Assets/Ship.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship{
+
+	public string name;
+	public Point position;
+
+	protected string _spritePath;
+	protected GameObject _instance = new GameObject();
+	protected SpriteRenderer _renderer = new SpriteRenderer();
+
+	public Ship(float x, float y, string name, string path){
+
+		position.x = x;
+		position.y = y;
+
+		this.name = name;
+		_spritePath = path;
+	}
+
+	public void Initialize(){
+
+		_instance.AddComponent<SpriteRenderer>();
+		_renderer = _instance.GetComponent<SpriteRenderer>();
+
+		_renderer.sprite = Resources.Load<Sprite>(_spritePath);
+
+		_instance.name = name;
+	}
+
+	public void Update(){
+
+		_instance.transform.position = new Vector3(position.x, position.y, 0);
+	}
+}
+
+public struct Point{
+
+	public float x;
+	public float y;
+
+	public Point(float x, float y){
+
+		this.x = x;
+		this.y = y;
+	}
+}
